4f9f215f3d497168c4f8bf0078474d5c3337aac2
aya49 <alice.ky@live.ca>
2022-12-28 19:04:58 -0800

    .

---
+++ 01_intro_to_R.R
@@ -0,0 +1,162 @@
+# Adding "#" at the beginning of this line makes this a comment!
+# Use comments to tell the reader what you are doing (documentation).
+# Comments are not run.
+
+## variables
+
+# numerics, used to perform arithmetics
+1 + 1
+2 * 3
+8 / 2
+(2 + 3) * 5
+
+x1 <- 5
+x1
+
+x1 <- 1 # replaces previously assignmed value, 5
+x1
+
+x2 <- 2
+x3 <- 1 + 2
+
+
+# strings, a sequence of characters
+s1 <- "one plus one"
+s2 <- "is"
+s3 <- paste0(s1, " ", s2, ": ", x3)
+
+# print a message to console
+print(x3)
+
+
+# vectors; indexing, length
+xv1 <- c(x1, x2, x3, 5)
+xv2 <- c(1:5)
+sv1 <- c(s1, s2, "last string")
+sv2 <- rep("a", 4) # rep = repeat
+
+xv1
+xv1[1]
+length(xv1)
+
+xv2
+xv2[2]
+length(xv2)
+
+sv1
+sv1[c(2:3)]
+length(sv1)
+
+sv2
+sv2[length(sv2)]
+length(sv2)
+
+## matrices; dimensions, column/row naming, indexing
+sm <- matrix("a", nrow=3, ncol=2)
+sm
+ncol(sm)
+nrow(sm)
+dim(sm) # dim=dimensions
+
+xm <- matrix(0, nrow=2, ncol=3)
+xm
+
+nm <- matrix(c(1:9), nrow=3, ncol=3)
+nm
+nmcolumns <- paste0("column", c(1:ncol(nm)))
+nmrows <- paste0("row", c(1:nrow(nm)))
+colnames(nm) <- nmcolumns
+rownames(nm) <- nmrows
+nm
+
+nm[2,1] # single numeric
+nm[c(1:2),2] # matrix
+nm[1,] # vector
+nm["row1",]
+nm[,2]
+nm[,"column2"]
+
+nm2 <- nm[,2] # vector
+length(nm2)
+
+nm2 <- nm[,2, drop=FALSE] # matrix
+dim(nm2)
+
+
+## if condition
+x1 <- 10
+
+if (x1 < 10) {
+  print("x1 is less than 10")
+}
+
+if (x1 <= 10) {
+  print("x1 is less than or equal to 10")
+}
+if (x1 == 10) {
+  print("x1 is 10")
+}
+if (x1 >= 10) {
+  print("x1 is greater than or equal to 10")
+}
+if (x1 > 10) {
+  print("x1 is greater than 10")
+}
+
+if (x1 < 10) {
+  print("x1 is less than 10")
+} else {
+  print("x1 is not less than 10")
+}
+
+if (x1 < 10) {
+  print("x1 is less than 10")
+} else if (x1 == 10) {
+  print("x1 is 10")
+} else {
+  print("x1 is greater than 10")
+}
+
+
+## loops: for, while
+for (i in c(3:10)) {
+  print(i)
+}
+
+i <- 3
+while (i <= 10) {
+  print(i)
+  i <- i + 1
+}
+
+
+## functions, a set of pre-written code
+## to see a description of the function, type "?" in front of it
+?print
+?length
+?matrix
+?dim
+?ncol
+?nrow
+?colnames
+?rownames
+
+# you can create your own function too!
+# encapsulation: everything that happens in a function stays in the function.
+myfunction <- function(p1, p2) {
+  p3 <- p1 * p2
+  return(p3)
+}
+
+p1 <- 3
+p2 <- 5
+p3 <- 7
+r1 <- myfunction(6, 8)
+
+p1
+p2
+p3
+r1
+
+# if you write many functions, you can bundle these functions into a "package"
+# and share it with the community :)
